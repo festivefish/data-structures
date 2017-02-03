11b4657a0b66c8df1143ba0e84db41a61ab960c5
festivefish <jonasmatt19@gmail.com>
2017-02-02 20:38:17 -0700

    Update Assignment2.cpp

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-bool Commonfilter(string *wordn, string *commonWords){
+bool Commonfilter(string wordn, string *commonWords){
 	bool isCommon = 0;
 	for (int i=0;i<50;i++){
 		if (wordn == commonWords[i]){
@@ -31,12 +31,17 @@ WordStruct currentWord;
 //initialize commonwords array
 string commonWords[50] = ("the", "you", "one", "be", "do", "all", "to", "at", "would", "of", "this", "there", "and", "but", "their", "a", "his", "what", "in", "by", "so", "that", "from", "up", "have", "they", "out", "i", "we", "if", "it", "say", "about", "for", "her", "who", "not", "she", "get", "on", "or", "which", "with", "an", "go", "he", "will", "me", "as", "my"); 
 
-int main(char *argv[]){
+int main(int argc, char *argv[]){
 
-bool unique = 1;
+cout<<"memes"<<endl;
 int size = 100;
-int *uniqueWords = new WordStruct[100];
-int *x;
+WordStruct *uniqueWords = new WordStruct[100];
+WordStruct *x;
+string line;
+bool common;
+int numUniqueWords = 0;
+int numDoublings = 0;
+int numCount = 0;
 
 char* filename = argv[1];   // filename = "HungerGames_edit.txt"
 int count = atoi(argv[2]);  // count = 20
@@ -47,15 +52,19 @@ myfile.open (filename);
 while (getline(myfile,line,' ')){
 	currentWord.word = line;
 
+	cout<<line<<endl;
+
 	//checks if word is a common word
-	common = Commonfilter(currentWord.word, commonWords)
+	common = Commonfilter(currentWord.word, commonWords);
+
+	bool unique = 1;
 
 	//if word is not common
 	if (common == 0){
 		//iterates through Unique words
 		for (int a=0;a<numUniqueWords;a++){
 			//checks if word is already listed as a unique word
-			if (currentWord.word = uniqueWords[a].word){
+			if (currentWord.word == uniqueWords[a].word){
 				//increases the number of times that word has been found
 				uniqueWords[a].numListings++;
 				//identifies the word as non-unique
